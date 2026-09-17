3a8640446bc15ea90c8b034e3507e9e8d4b363fd
Moutialaarfaoui-star <moutialaarfaoui@gmail.com>
2026-09-17 11:33:10 +0100

    Variables & types

--- day1/challenge1.js
+++ day1/challenge1.js
@@ -1,10 +1,13 @@
+const prompt = require('prompt-sync')();
 
-let nom = "laarfaoui"
-let prenom = "moutia"
-let age = 18
-let sexe = "male"
-let email = "moutialaarfaoui@gmail.com"
+// 1. Saisie des informations
+let nom = prompt("Quel est votre nom ?");
+let prenom = prompt("Quel est votre prénom ?");
+let age = prompt("Quel est votre âge ?");
+let sexe = prompt("Quel est votre sexe (M/F) ?");
+let email = prompt("Quelle est votre adresse e-mail ?");
 
+// 2. Affichage du résultat
 console.log(`Voici vos informations :
 - Nom : ${nom}
 - Prénom : ${prenom}
