5487ab3125303a5e6644b91708a7a089e8079ccd
Moutialaarfaoui-star <moutialaarfaoui@gmail.com>
2026-09-17 11:13:53 +0100

    the all challenge of days

---
+++ day1/challenge1.js
@@ -0,0 +1,13 @@
+
+let nom = "laarfaoui"
+let prenom = "moutia"
+let age = 18
+let sexe = "male"
+let email = "moutialaarfaoui@gmail.com"
+
+console.log(`Voici vos informations :
+- Nom : ${nom}
+- Prénom : ${prenom}
+- Âge : ${age} ans
+- Sexe : ${sexe}
+- E-mail : ${email}`);
